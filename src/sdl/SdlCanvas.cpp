82f9bc061d5e23831cae835665d143e7bd37ebb0
Alexis Cheron <git@norech.com>
2022-04-06 15:26:12 +0400

    fixed sdl colors

--- src/sdl/SdlCanvas.cpp
+++ src/sdl/SdlCanvas.cpp
@@ -40,12 +40,19 @@ void SdlCanvas::drawPoint(int x, int y, const IColor& color)
     SDL_RenderDrawRect(_graphic->_renderer, &rect);
 }
 
-void SdlCanvas::drawText(int x [[maybe_unused]], int y [[maybe_unused]], const std::string &text [[maybe_unused]], const IColor &foreColor [[maybe_unused]])
+void SdlCanvas::drawText(int x [[maybe_unused]], int y [[maybe_unused]],
+    const std::string& text [[maybe_unused]],
+    const IColor& foreColor [[maybe_unused]])
 {
-    SDL_Color color = {static_cast<Uint8>((foreColor.getColorCode() & 0xFF0000) << 16), static_cast<Uint8>((foreColor.getColorCode() & 0xFF0000) << 8), static_cast<Uint8>((foreColor.getColorCode() & 0xFF0000)), 255};
-    SDL_Surface *surface = TTF_RenderText_Solid(_graphic->_font, text.c_str(), color);
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(_graphic->_renderer, surface);
-    SDL_Rect trect = {(x * 20) + 3, (y * 20) - 3, 0, 0};
+    SDL_Color color
+        = { static_cast<Uint8>((foreColor.getColorCode() & 0xFF0000) >> 16),
+              static_cast<Uint8>((foreColor.getColorCode() & 0x00FF00) >> 8),
+              static_cast<Uint8>((foreColor.getColorCode() & 0x0000FF)), 255 };
+    SDL_Surface* surface
+        = TTF_RenderText_Solid(_graphic->_font, text.c_str(), color);
+    SDL_Texture* texture
+        = SDL_CreateTextureFromSurface(_graphic->_renderer, surface);
+    SDL_Rect trect = { (x * 20) + 3, (y * 20) - 3, 0, 0 };
 
     SDL_QueryTexture(texture, NULL, NULL, &trect.w, &trect.h);
     SDL_RenderCopy(_graphic->_renderer, texture, NULL, &trect);
