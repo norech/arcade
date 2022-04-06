3997037052af5dd174aea293356322a7c2bd6d58
Alexis Cheron <git@norech.com>
2022-04-06 10:12:43 +0400

    deferred closing of window in libcaca

--- src/libcaca/CacaGraphic.cpp
+++ src/libcaca/CacaGraphic.cpp
@@ -7,70 +7,51 @@
 
 #include "CacaGraphic.hpp"
 #include "CacaCanvas.hpp"
-#include <iostream>
-#include <thread>         // std::this_thread::sleep_for
 #include <chrono>
+#include <iostream>
+#include <thread> // std::this_thread::sleep_for
 
 namespace arc::grph {
 
-CacaGraphic::CacaGraphic()
-{
+CacaGraphic::CacaGraphic() { }
 
-}
-
-CacaGraphic::~CacaGraphic()
-{
-
-}
+CacaGraphic::~CacaGraphic() { }
 
 void CacaGraphic::init()
 {
     _display = caca_create_display(NULL);
-    //if (_display == NULL) {
+    // if (_display == NULL) {
     //    throw std::runtime_error("Can't create display");
     //}
     _canvas = caca_get_canvas(_display);
-    //if (_canvas == NULL) {
+    // if (_canvas == NULL) {
     //    throw std::runtime_error("Can't get canvas");
     //}
     caca_set_display_title(_display, "Arcade");
-    caca_set_display_time(_display, 250);
+    caca_set_display_time(_display, 25000);
     caca_set_color_ansi(_canvas, CACA_BLACK, CACA_WHITE);
 }
 
 bool CacaGraphic::isOpen()
 {
+    if (_willBeClosed)
+        return false;
     if (_display != NULL) {
-        return (true);
+        return true;
     } else {
-        return (false);
+        return false;
     }
 }
 
-void CacaGraphic::close()
-{
-    caca_free_display(_display);
-}
+void CacaGraphic::close() { _willBeClosed = true; }
 
+void CacaGraphic::clear() { caca_clear_canvas(_canvas); }
 
-void CacaGraphic::clear()
-{
-    caca_clear_canvas(_canvas);
-}
+void CacaGraphic::render() { caca_refresh_display(_display); }
 
-void CacaGraphic::render()
-{
-    caca_refresh_display(_display);
-    std::this_thread::sleep_for(std::chrono::milliseconds(25));
-}
+float CacaGraphic::tick() { return 0.025; }
 
-float CacaGraphic::tick()
-{
-    return 0.025;
-}
-
-
-bool CacaGraphic::pollEvent(Event &event)
+bool CacaGraphic::pollEvent(Event& event)
 {
     caca_event_t ev;
 
@@ -88,50 +69,45 @@ bool CacaGraphic::pollEvent(Event &event)
     return false;
 }
 
-
-void CacaGraphic::loadCanvas(std::shared_ptr<ICanvas> &canvas)
+void CacaGraphic::loadCanvas(std::shared_ptr<ICanvas>& canvas)
 {
     canvas = std::make_shared<CacaCanvas>(this);
 }
 
-void CacaGraphic::unloadCanvas(std::shared_ptr<ICanvas> &canvas)
+void CacaGraphic::unloadCanvas(std::shared_ptr<ICanvas>& canvas)
 {
     canvas.reset();
 }
 
-void CacaGraphic::destroy()
-{
-    close();
-}
+void CacaGraphic::destroy() { caca_free_display(_display); }
 
-void CacaGraphic::registerSprite(game::ISprite &sprite [[maybe_unused]])
+void CacaGraphic::registerSprite(game::ISprite& sprite [[maybe_unused]])
 {
     return;
 }
 
-int CacaGraphic::getCacaColor(const IColor &code)
+int CacaGraphic::getCacaColor(const IColor& code)
 {
     switch (code.getColorCode()) {
-        case ColorCode::BLACK:
-            return CACA_BLACK;
-        case ColorCode::RED:
-            return CACA_RED;
-        case ColorCode::GREEN:
-            return CACA_GREEN;
-        case ColorCode::YELLOW:
-            return CACA_YELLOW;
-        case ColorCode::BLUE:
-            return CACA_BLUE;
-        case ColorCode::MAGENTA:
-            return CACA_MAGENTA;
-        case ColorCode::CYAN:
-            return CACA_CYAN;
-        case ColorCode::WHITE:
-            return CACA_WHITE;
-        default:
-            return CACA_BLACK;
+    case ColorCode::BLACK:
+        return CACA_BLACK;
+    case ColorCode::RED:
+        return CACA_RED;
+    case ColorCode::GREEN:
+        return CACA_GREEN;
+    case ColorCode::YELLOW:
+        return CACA_YELLOW;
+    case ColorCode::BLUE:
+        return CACA_BLUE;
+    case ColorCode::MAGENTA:
+        return CACA_MAGENTA;
+    case ColorCode::CYAN:
+        return CACA_CYAN;
+    case ColorCode::WHITE:
+        return CACA_WHITE;
+    default:
+        return CACA_BLACK;
     }
 }
 
 }
-
